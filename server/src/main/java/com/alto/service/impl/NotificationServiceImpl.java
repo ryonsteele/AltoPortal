5802b569e17bf9fe1b6f7818aceb35370a7d0771
Ryon Steele <jeffery.steele@eversana.com>
2020-03-22 10:09:23 -0400

    email debugging

--- server/src/main/java/com/alto/service/impl/NotificationServiceImpl.java
+++ server/src/main/java/com/alto/service/impl/NotificationServiceImpl.java
@@ -8,8 +8,10 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 
 @Service
@@ -24,25 +26,60 @@ public class NotificationServiceImpl implements NotificationService {
 
     MimeMessage mimeMessage = mailSender.createMimeMessage();
 
+
+    Properties props = new Properties();
+    props.put("mail.smtp.host", "192.168.1.10");
+    props.put("mail.smtp.socketFactory.port", "465");
+    props.put("mail.smtp.socketFactory.class",
+            "javax.net.ssl.SSLSocketFactory");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.port", "25");
+    Session session = Session.getDefaultInstance(props,
+            new javax.mail.Authenticator() {
+              @Override
+              protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("webapps@altostaffing.com","W3b@pps");
+              }
+            });
+
     try {
 
-      MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+      Message message = new MimeMessage(session);
+      message.setFrom(new InternetAddress("test@gmail.com"));
+      message.setRecipients(Message.RecipientType.TO,
+              InternetAddress.parse("ryonsteele@gmail.com"));
+      message.setSubject("Testing Subject");
+      message.setText("Test Mail");
 
-      mimeMessageHelper.setSubject("Sent Home Alert");
-      mimeMessageHelper.setFrom("webapps@altostaffing.com");
-      mimeMessageHelper.setTo("ryonsteele@gmail.com");
-      mimeMessageHelper.setText("User: " + request.getUsername() + " TempID: " + request.getTempId() +
-              " is reporting being sent home from Client: " + request.getClientName());
+      Transport.send(message);
 
-      mailSender.send(mimeMessageHelper.getMimeMessage());
+      System.out.println("Done");
 
-    } catch (MessagingException e) {
-      e.printStackTrace();
-      return false;
     } catch (Exception e) {
+      //throw new RuntimeException(e);
       e.printStackTrace();
       return false;
     }
+
+//    try {
+//
+//      MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+//
+//      mimeMessageHelper.setSubject("Sent Home Alert");
+//      mimeMessageHelper.setFrom("webapps@altostaffing.com");
+//      mimeMessageHelper.setTo("ryonsteele@gmail.com");
+//      mimeMessageHelper.setText("User: " + request.getUsername() + " TempID: " + request.getTempId() +
+//              " is reporting being sent home from Client: " + request.getClientName());
+//
+//      mailSender.send(mimeMessageHelper.getMimeMessage());
+//
+//    } catch (MessagingException e) {
+//      e.printStackTrace();
+//      return false;
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//      return false;
+//    }
     return true;
   }
 
