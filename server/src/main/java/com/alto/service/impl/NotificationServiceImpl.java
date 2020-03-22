3ab42b677fb6803797f5edad823611c404672955
Ryon Steele <jeffery.steele@eversana.com>
2020-03-22 10:27:34 -0400

    email debugging

--- server/src/main/java/com/alto/service/impl/NotificationServiceImpl.java
+++ server/src/main/java/com/alto/service/impl/NotificationServiceImpl.java
@@ -30,7 +30,7 @@ public class NotificationServiceImpl implements NotificationService {
     Properties props = new Properties();
     props.put("mail.smtp.host", "192.168.1.10");
     props.put("mail.smtp.socketFactory.port", "25");
-    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.auth", "false");
     props.put("mail.smtp.port", "25");
     props.put("mail.debug", "true");
     Session session = Session.getDefaultInstance(props,
